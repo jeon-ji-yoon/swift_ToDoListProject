7e7f4f21c5ce26178c528be9b4a60be386b9b15b
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-20 17:21:27 +0900

    Initial Commit

---
+++ ToDoList1/ToDoList1App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList1App.swift
+//  ToDoList1
+//
+//  Created by hjins :) on 2021/01/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
