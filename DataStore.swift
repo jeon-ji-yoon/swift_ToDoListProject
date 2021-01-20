4338d5025ab99054509a2dfdff1b9a8fda808466
jeon-ji-yoon <appleru1515@gmail.com>
2021-01-20 17:33:24 +0900

    입력받은 데이터( 할 일 ) 를 저장할 수 있도록 하는 swift 파일을 만든다

---
+++ DataStore.swift
@@ -0,0 +1,19 @@
+//
+//  DataStore.swift
+//  ToDoList1
+//
+//  Created by hjins :) on 2021/01/20.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct Task : Identifiable {
+    var id = String()
+    var toDoItem = String()
+}
+
+class TaskStore : ObservedObject {
+    @Published var tasks = [Task]()
+}
